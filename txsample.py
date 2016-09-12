5db0be0c156f87f17a62d0a2d35c3a1c9a43b5d9
NaotakaSaito <saitou088@dsn.lapis-semi.com>
2016-09-13 00:41:15 +0900

    under development

---
+++ txsample.py
@@ -0,0 +1,20 @@
+from PyLaz.PyLaz import *
+
+lazurite = PyLaz()
+
+result = lazurite.open()
+print(result)
+
+result = lazurite.begin(36,0xabcd,100,20)
+print(result)
+
+result = lazurite.send(0xabcd,0xffff,"hello")
+print(result)
+
+result = lazurite.close()
+print(result)
+
+result = lazurite.remove()
+print(result)
+
+
